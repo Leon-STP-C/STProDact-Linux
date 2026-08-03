80bca69f3787977fa304ce49d4f8ca5834b523d1
Leon-STP-C <leon.pemmer@stpcontrols.com>
2026-08-03 08:54:34 +0200

    Add more documentation in README.md

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # STProDact-Linux
 
-The purpose of this project is to try to provide an automated Setup for STProDact for Linux-Systems via the usage of Docker Containers.
+The purpose of this project is to try to provide an automated Setup for STProDact for Linux-Systems via the usage of Docker Containers. The handling of dependencies in the environment is mainly done via the flake.nix which is being run through the .envrc. Make sure direnv is installed and properly set up for your shell for it to work.
+
+# Manual Prequisites
+Since flake.nix handles most dependencies this requires Nix to be installed on the system. You can download the Nix package manager [here](https://nixos.org/download/)\
+For this to work direnv also has to be setup on the system. Basic Installation guide can be found [here](https://github.com/direnv/direnv#basic-installation)
+
+**IMPORTANT!**\
+While the flake.nix handles dependencies the docker daemon still needs to be installed and setup manually for your distribution.
+
+
--- flake.nix
+++ flake.nix
@@ -6,20 +6,45 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
 
           buildInputs = with pkgs; [
             nodejs
+
+            nixfmt
+            nixd
+            docker
+            docker-compose
           ];
 
           shellHook = ''
             echo "Dev environment ready"
+
+            if ! docker info >/dev/null 2>&1; then
+              if groups | grep -q docker; then
+                echo "⚠️  Docker daemon doesn't seem to be running."
+                echo "    Try: sudo systemctl start docker"
+              else
+                echo "⚠️  You're not in the 'docker' group yet (or session hasn't refreshed)."
+                echo "    Try: sudo usermod -aG docker \$USER && newgrp docker"
+              fi
+            else
+              echo "✅ Docker daemon is running."
+            fi
           '';
         };
-      });
+      }
+    );
 }
